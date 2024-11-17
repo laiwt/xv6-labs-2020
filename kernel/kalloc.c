3ef0e61338e495b1894a5f2b486fc71fc2dd38de
root <root@LAPTOP-SS8H83O6>
2024-11-17 21:41:27 +0800

    Second lab

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,18 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+int 
+get_freemem()
+{
+  struct run *r;
+  int res = 0;
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  while (r) {
+    res += PGSIZE;
+    r = r->next;
+  }
+  release(&kmem.lock);
+  return res;
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,8 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  p->syscall_trace = 0;
+  
   return p;
 }
 
@@ -290,6 +292,8 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
+
+  np->syscall_trace = p->syscall_trace;
 
   pid = np->pid;
 
@@ -692,4 +696,18 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+int 
+get_nproc()
+{
+  struct proc *p;
+  int res = 0;
+  for(p = proc; p < &proc[NPROC]; p++){
+    if(p->state == UNUSED) {
+      continue;
+    }
+    ++res;
+  }
+  return res;
 }
